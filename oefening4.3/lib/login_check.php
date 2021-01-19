e42256f4e7917f1928bb8e47cb70304c57cb1a18
Petervrancken <peter.vrancken88@gmail.com>
2021-01-20 00:39:19 +0100

    4.3 rdy

--- oefening4.3/lib/login_check.php
+++ oefening4.3/lib/login_check.php
@@ -9,37 +9,57 @@ if ( LoginCheck() ) {
     print "Helaas, het ingloggen is niet gelukt.";
 }
 
-
-function loginCheck()
+function LoginCheck():bool
 {
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if ( $_SERVER['REQUEST_METHOD'] == "POST" )
+    {
         //controle CSRF token
-       //if (!key_exists("csrf", $_POST)) die("Missing CSRF");
-       //if (!hash_equals($_POST['csrf'], $_SESSION['last_csrf'])) die("Problem with CSRF");
+        if ( ! key_exists("csrf", $_POST)) die("Missing CSRF");
+        if ( ! hash_equals( $_POST['csrf'], $_SESSION['lastest_csrf'] ) ) die("Problem with CSRF");
 
+        $_SESSION['lastest_csrf'] = "";
 
         //sanitization
-
         $_POST = StripSpaces($_POST);
         $_POST = ConvertSpecialChars($_POST);
 
         //validation
+        $sending_form_uri = $_SERVER['HTTP_REFERER']; // http_referer, zien waar we vandaan komen
 
-        $sending_form_uri = $_SERVER['HTTP_REFERER'];
-        $email = $_POST['usr_email'];
-        $password = $_POST['usr_password'];
-
-        //Protect MySQL injection
-
-
-
-        //Check if user exists
-
-        $data = getData("SELECT usr_email FROM user WHERE usr_email = '".$email."'");
-        if($data != null ){
-            return true;
-        } else {
-            return false;
+        //Validaties voor het loginformulier
+        // als er geen key usr_email in $_post zit + extra controle op lengte. Geef een foutmelding.
+        if ( ! key_exists("usr_email", $_POST ) OR strlen($_POST['usr_email']) < 5 )
+        {
+            $_SESSION['errors']['usr_password'] = "Het wachtwoord is niet correct ingevuld";
         }
+        if ( ! key_exists("usr_password", $_POST ) OR strlen($_POST['usr_password']) < 8 )
+        {
+            $_SESSION['errors']['usr_password'] = "Het wachtwoord is niet correct ingevuld";
+        }
+
+        //terugkeren naar afzender als er een fout is
+        if ( key_exists("errors" , $_SESSION ) AND count($_SESSION['errors']) > 0 )
+        {   // als er een foutmelding is terugkeren naar vorige pagina en
+            $_SESSION['OLD_POST'] = $_POST;
+            header( "Location: " . $sending_form_uri ); exit(); // als foutmelding terugkeren naar vorige pagina,
+                                                                    //exit() = stop lezen van de code.
+        }
+
+        //search user in database
+        $email = $_POST['usr_email'];
+        $ww = $_POST['usr_password'];
+
+        $sql = "SELECT * FROM user WHERE usr_email='$email' ";
+        $data = GetData($sql);
+
+        if ( count($data) > 0 )
+        {
+            foreach ( $data as $row )
+            {
+                if ( password_verify( $ww, $row['usr_password'] ) ) return true;
+            }
+        }
+
+        return false;
     }
 }
